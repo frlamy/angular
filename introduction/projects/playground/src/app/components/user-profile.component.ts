2d54c58cee19abe6e11f6b3a8c7fc12061e758f8
Franck LAMY <franck.lamy@neweb.com>
2024-12-08 15:05:41 +0100

    Ending components and directives set ups

--- introduction/projects/playground/src/app/components/user-profile.component.ts
+++ introduction/projects/playground/src/app/components/user-profile.component.ts
@@ -1,29 +1,41 @@
-import {Component, HostListener, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'user-profile',
   standalone: false,
   template: `
+    <!--  CLASS BINDING : Quand on ajoute dynamiquement une class sur un élément html, en fonction d'une condition JS  -->
     <h3 [class.hired]="isHired" [class.not-hired]="!isHired">{{ firstName | titlecase }} {{ lastName | uppercase }}</h3>
+
+    <!--    PROPERTY BINDING : Quand un attribut est entouré de crochet, c'est une valeur JS, on peut y mettre une variable, une fonction, une ternaire-->
     <img [src]="avatar" alt="user-picture">
     Job : <strong>{{ job | titlecase }} ({{ salary | currency :'EUR' : 'symbol' }} / mois)</strong>
+
     <button *ngIf="!isHired" (click)="onClickButton()">Hire</button>
+
+    <!--  EVENT BINDING : Quand on lie une fonction directement à un event du template  -->
     <button *ngIf="isHired" (click)="onClickButton()">Reject</button>
-    <input (keydown.enter)="onEnterKeyDown($event)" (keydown.backspace)="onBackSpaceKeyDown($event)" type="text" placeholder="New name">
+
+    <!-- VARIABLE DE TEMPLATE : A utiliser avec @ViewChild, et utilisable uniquement après l'utilisation de la vue => Cf. ngAfterViewInit(){}   -->
+    <input #newFirstName (keydown.space)="onSpaceKeyDown($event)" (keydown.enter)="changeFirstName()" type="text" placeholder="New name">
+
+    <button (click)="changeFirstName()" >Change name</button>
+
     <hr>
   `,
-  styles: [`
-    .hired {
-      color: green;
-    }
-
-    .not-hired {
-      color: red;
-    }
-  `]
+  styleUrl: '../css/user-profile.component.css'
 })
 export class UserProfileComponent {
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    if (this.newFirstName) {
+      this.newFirstName.nativeElement.value = 'Jake';
+    }
+  }
+
+  @ViewChild('newFirstName')
+  newFirstName ?: ElementRef<HTMLInputElement>;
 
   @Input('first-name')
   firstName: string = '';
@@ -46,11 +58,14 @@ export class UserProfileComponent {
     this.isHired = !this.isHired;
   }
 
-  onEnterKeyDown(event: Event) {
-      console.log('Enter is triggered');
+  changeFirstName() {
+    const newValue: string|undefined = this.newFirstName?.nativeElement?.value;
+    if (newValue) {
+      this.firstName = newValue;
+    }
   }
 
-  onBackSpaceKeyDown(event: Event) {
+  onSpaceKeyDown(event: Event) {
     console.log('Backspace is triggered');
   }
 }
