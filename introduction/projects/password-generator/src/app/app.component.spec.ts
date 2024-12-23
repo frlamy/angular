f115be5fdd86170683935727050c491db4b226a1
Franck LAMY <franck.lamy@neweb.com>
2024-12-23 22:10:06 +0100

    Refacto tests with Spectator

--- introduction/projects/password-generator/src/app/app.component.spec.ts
+++ introduction/projects/password-generator/src/app/app.component.spec.ts
@@ -1,7 +1,52 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {Spectator, createComponentFactory} from '@ngneat/spectator';
 
-describe('AppComponent', () => {
+describe('AppComponent (Spectator)', () => {
+  let spectator: Spectator<AppComponent>;
+
+  let component: AppComponent;
+
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    declarations: [AppComponent]
+  });
+
+  beforeEach(() => {
+    spectator = createComponent();
+    component = spectator.component;
+  });
+
+  it('should work', () => {
+    expect(spectator.query('article')).toHaveText('Cliquez sur le bouton "générer"');
+  });
+
+  it('should change message when button is clicked and be : GENERATED_PASSWORD', async () => {
+    spectator.click('button');
+    expect(spectator.query('article')?.textContent).toBe('GENERATED_PASSWORD');
+  });
+
+  it('should update settings when checkboxes are clicked', async () => {
+
+    spectator.click('#uppercase');
+    expect(component.uppercase).toBeTrue();
+
+    spectator.click('#numbers');
+    expect(component.numbers).toBeTrue();
+
+    spectator.click('#symbols');
+    expect(component.symbols).toBeTrue();
+  })
+
+  it('should update settings when value is updated', async () => {
+    // When it's an input, we have to call dispatchEvent(new Event('input'));
+    spectator.typeInElement("33", "#length");
+    expect(component.length).toEqual(33);
+  })
+
+});
+
+describe('AppComponent (TestBed)', () => {
   let fixture: ComponentFixture<AppComponent>;
 
   let component: AppComponent;
@@ -20,7 +65,6 @@ describe('AppComponent', () => {
 
   it('should work', async () => {
     const article = fixture.nativeElement.querySelector('article');
-
     expect(article.textContent).toBe('Cliquez sur le bouton "générer"');
   });
 
